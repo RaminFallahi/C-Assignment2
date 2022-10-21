acf480f1043ea5cd5d1d694636173fd35cb108c0
liferaam <miticomanly@gmail.com>
2022-10-20 20:27:19 -0400

    first commit

---
+++ assignment2/Controllers/J1Controller.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace assignment2.Controllers
+{
+
+    public class J1Controller : ApiController
+    {
+        /// <summary>
+        /// </summary>
+        //<example>
+        //api/J1/Menu/{burger}/... ===> api/J1/menu/1/0/0/0 ====> 461
+        //<example>
+        /// <returns>returns the totaCalory of the user input (0,1,2,3)</returns>
+        /// <example>
+        //api/J1/Menu/{burger}/... ===> api/J1/menu/1/1/1/1
+        [HttpGet]
+        [Route("api/J1/Menu/{burger}/{drink}/{side}/{dessert}")]
+        public int Menu(int burger, int drink, int side, int dessert)
+        {
+            
+            int totalCalory = 0;
+
+            if (burger == 1)
+            {
+                totalCalory += 461;
+            }
+            else if (burger == 2)
+            {
+                totalCalory += 431;
+            }
+            else if (burger == 3)
+            {
+                totalCalory += 420;
+            }
+
+
+            if (side == 1)
+            {
+                totalCalory += 100;
+            }
+            else if (side == 2)
+            {
+                totalCalory += 57;
+            }
+            else if (side == 3)
+            {
+                totalCalory += 70;
+            }
+            
+            
+            if (drink == 1)
+            {
+                totalCalory += 130;
+            }
+            else if (drink == 2)
+            {
+                totalCalory += 160;
+            }
+            else if (drink == 3)
+            {
+                totalCalory += 118;
+            }
+            
+            
+            if (dessert == 1)
+            {
+                totalCalory += 167;
+            }
+            else if (dessert == 2)
+            {
+                totalCalory += 266;
+            }
+            else if (dessert == 3)
+            {
+                totalCalory += 75;
+            }
+
+            return totalCalory;
+        }
+    }
+}
+
---
+++ assignment2/Controllers/J2Controller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace assignment2.Controllers
+{
+    public class J2Controller : ApiController
+    {
+        /// <summary>
+        /// Write a program which determines how many ways you can roll 2 dices(m&n) the value of 10.
+        /// </summary>
+        //<example>
+        //api/J2/DiceGame/5/5 ... ===>   There is 1 way to get the sum 10.
+        //<example>
+        /// <returns>returns the possibility of the reaching 10 by rolling two dices M & N</returns>
+        /// <example>
+        //api/J2/DiceGame/5/5... ===> api/J2/DiceGame/5/5
+        [HttpGet]
+        [Route("api/J2/DiceGame/{n}/{m}")]
+        public int DiceGame(int n, int m)
+        {
+            int tenCounter = 0;
+            for (int i = 0; i <= n; i++) 
+            {
+                for (int j = 0; j <= m; j++) 
+                {
+                    if (i + j == 10) 
+                    tenCounter++;
+                }
+            } return tenCounter;
+        }
+    }
+}
